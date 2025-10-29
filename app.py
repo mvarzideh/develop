f804d6b79d25a02b0781c304e0dc86bae19058eb
mvarzideh <mvarzideh@gmail.com>
2025-10-29 11:27:46 +0330

    add new feature

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     x = int(input("Enter first number: "))
     y = int(input("Enter second number: "))
     print("Sum:", add(x, y))
+    print("Product:", multiply(x, y))
 
